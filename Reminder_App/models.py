0bbc329f49f57d4d8a104980fabb9f78d51688c7
TejasVarute <tvarute@gmail.com>
2025-09-21 21:51:01 +0530

    update

--- Reminder_App/models.py
+++ Reminder_App/models.py
@@ -7,7 +7,7 @@ class Reminder(models.Model):
     location = models.CharField(default="")
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
-    user = models.ForeignKey(UserData, on_delete=models.CASCADE, default="")
+    user = models.ForeignKey(UserData, on_delete=models.CASCADE)
     
     def __str__(self):
         return f'{self.message} @ {self.location} @ {self.date_time}'
@@ -17,7 +17,7 @@ class PastReminder(models.Model):
     location = models.CharField(default="")
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
-    user = models.ForeignKey(UserData, on_delete=models.CASCADE, default="")
+    user = models.ForeignKey(UserData, on_delete=models.CASCADE)
     
     def __str__(self):
         return f'{self.message} @ {self.location} @ {self.date_time}'
--- static/script.js
+++ static/script.js
@@ -4,20 +4,3 @@ function showForm() {
     form.classList.toggle("show");
 }
 
-// Login form toggler script
-function changeForm() {
-    const login = document.getElementById('login');
-    const register = document.getElementById('register');
-    const loginVisible = window.getComputedStyle(login).display !== "none";
-    const button = document.getElementById('toggle-button');
-
-    if (loginVisible) {
-        login.style.display = "none";
-        register.style.display = "block";
-        button.textContent = "Login";
-    } else {
-        login.style.display = "block";
-        register.style.display = "none";
-        button.textContent = "Register";
-    }
-}
